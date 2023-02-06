a528b13b4111eca3a0bdf9b9e67ad6e2029025a0
NaveenSankuri <husurpushpasankuri@gmail.com>
2023-02-06 14:38:23 +0530

    version 0.2

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 const express = require('express')
 const app = express();
-const mustacheExpress = require('mustache-express');
-const path = require('path')
-const bodyParser = require('body-parser');
-
-
-
-app.engine('mustache', mustacheExpress());
-app.set('views', './views');
-app.set('view engine', 'mustache');
-
-app.post('/incre', (req, res) => {
-    var n1 = parseInt(req.body.x);
-
-    var result = n1 + 1;
-
-    res.send('value:' + result);
+app.use('/', (req, res) => {
+    res.send("depoloyed");
 })
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-app.use('/css', express.static(path.join(__dirname, 'css')))
+// const mustacheExpress = require('mustache-express');
+// const path = require('path')
+// const bodyParser = require('body-parser');
 
 
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+// app.engine('mustache', mustacheExpress());
+// app.set('views', './views');
+// app.set('view engine', 'mustache');
+
+// app.post('/incre', (req, res) => {
+//     var n1 = parseInt(req.body.x);
+
+//     var result = n1 + 1;
+
+//     res.send('value:' + result);
+// })
+
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// app.use('/css', express.static(path.join(__dirname, 'css')))
+
+
+
+// app.get('/', (req, res) => {
+//     res.render('home');
+// })
 
 
 
